debbd20cfb7ba769f96636fe92bfaa1cfc69df72
ardagnr <arda.fusjn@gmail.com>
2024-11-06 00:24:11 +0300

    Add project files.

---
+++ Circle/Controllers/CircleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Circle.Models;
+
+namespace Circle.Controllers
+{
+    public class CircleController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View(new CircleModel());
+        }
+
+
+        [HttpPost]
+        public IActionResult Calculate(CircleModel model)
+        {
+            double pi = 3.14159;
+
+
+            if (model.Radius <= 0)
+            {
+                ViewData["ErrorMessage"] = "Yaricap değeri pozitif bir sayi olmali";
+                return View("Index", model);
+            }
+
+            model.Area = pi * model.Radius * model.Radius;
+
+
+            ViewData["ErrorMessage"] = null;
+
+            return View("Index", model);
+        }
+
+
+
+    }
+}
---
+++ Circle/Models/CircleModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Circle.Models
+{
+    public class CircleModel
+    {
+        public double Radius { get; set; }
+        public double Area { get; set; }
+    }
+}
